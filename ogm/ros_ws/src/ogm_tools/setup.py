5cf9d4dac5b31212fbc4fadcfa5fc9fa32f6653b
trung.l.nguyen <trung.l.nguyen@tuni.fi>
2025-12-15 23:44:45 +0200

    add ogm tools

---
+++ ogm/ros_ws/src/ogm_tools/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'ogm_tools'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='nvidia',
+    maintainer_email='trung.l.nguyen@tuni.fi',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'map_crop_memory = ogm_tools.map_crop_memory:main',
+        ],
+    },
+)
