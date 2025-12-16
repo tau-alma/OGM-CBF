ad7b025ddd14c9a6042c0846fb1ea4e7cea87670
Milos Pragr <milos.pragr@tuni.fi>
2025-12-16 18:40:42 +0200

    scan transformer humble compilable

--- ogm/ros_ws/src/ogm_gridmap/include/laserscan_transformer_node.h
+++ ogm/ros_ws/src/ogm_gridmap/include/laserscan_transformer_node.h
@@ -3,6 +3,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 
@@ -23,18 +24,18 @@ class TransformerNode  : public rclcpp::Node
     message_filters::Subscriber<nav_msgs::msg::Odometry> sub_odom;
     std::shared_ptr<message_filters::Synchronizer<
       message_filters::sync_policies::ApproximateTime<
-      sensor_msgs::msg::PointCloud2,
+      sensor_msgs::msg::LaserScan,
       nav_msgs::msg::Odometry>>> sync;
 
 
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+    //std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
+    //std::unique_ptr<tf2_ros::Buffer> tf_buffer;
 
-    //std::string target_frame;
-    //std::string link_frame;
-    //std::string sensor_frame;
+    std::string target_frame;
+    std::string link_frame;
+    std::string sensor_frame;
 
-    Eigen::Matrix4f T_link2sensor = Eigen::Matrix4f::Identity();
+    //Eigen::Matrix4f T_link2sensor = Eigen::Matrix4f::Identity();
 
     /*
     Eigen::Matrix4f get_T_matrix(std::shared_ptr<geometry_msgs::msg::TransformStamped> transform)
@@ -65,7 +66,7 @@ class TransformerNode  : public rclcpp::Node
       return T;
 
     }*/
-
+    /*
     Eigen::Matrix4f get_T_matrix(nav_msgs::msg::Odometry::ConstSharedPtr odom)
     {
       Eigen::Matrix4f T = Eigen::Matrix4f::Identity();
@@ -91,7 +92,6 @@ class TransformerNode  : public rclcpp::Node
       return T;
     }
 
-    /*
     void get_link2sensor()
     {
       std::shared_ptr<geometry_msgs::msg::TransformStamped> transform(nullptr);
@@ -130,21 +130,18 @@ class TransformerNode  : public rclcpp::Node
 
 
     void callback_scan_registration(
-        const sensor_msgs::msg::PointCloud2::ConstSharedPtr msg_scan,
+        const sensor_msgs::msg::LaserScan::ConstSharedPtr msg_scan,
         const nav_msgs::msg::Odometry::ConstSharedPtr msg_odom
         )
     {
-      rclcpp::Time scan_stamp(msg_pcd->header.stamp);
+      rclcpp::Time scan_stamp(msg_scan->header.stamp);
       rclcpp::Time odom_stamp(msg_odom->header.stamp);
       RCLCPP_DEBUG(this->get_logger(), "sync %lf %lf", scan_stamp.seconds(), odom_stamp.seconds());
 
-      Eigen::Matrix4f T_map2link = get_T_matrix(msg_odom);     
-
-      Eigen::Matrix4f T_map2sensor = T_map2link * T_link2sensor;
   
       sensor_msgs::msg::LaserScan msg_out;
 
-      msg_out = *msg_scan;
+      //msg_out = *msg_scan;
       msg_out.header.frame_id = target_frame;
       pub_scan->publish(msg_out);
     }
@@ -161,26 +158,26 @@ class TransformerNode  : public rclcpp::Node
       sensor_frame = this->declare_parameter("sensor_frame", "base_sensor");
       RCLCPP_INFO(this->get_logger(), "sensor_frame: %s", sensor_frame.c_str());
 
-      get_link2sensor();
+      //get_link2sensor();
 
-      pub_pc2 = this->create_publisher<sensor_msgs::msg::PointCloud2>("pcd_transformed", 1);
+      pub_scan = this->create_publisher<sensor_msgs::msg::LaserScan>("scan_transformed", 1);
 
       rclcpp::QoS qos = rclcpp::QoS(10);
-      sub_pcd.subscribe(this, "pcd", qos.get_rmw_qos_profile());
-      sub_pose.subscribe(this, "pose", qos.get_rmw_qos_profile());
+      sub_scan.subscribe(this, "scan", qos.get_rmw_qos_profile());
+      sub_odom.subscribe(this, "pose", qos.get_rmw_qos_profile());
 
       uint32_t queue_size = 100;
       sync = std::make_shared<message_filters::Synchronizer<
         message_filters::sync_policies::ApproximateTime<
-        sensor_msgs::msg::PointCloud2,
+        sensor_msgs::msg::LaserScan,
         nav_msgs::msg::Odometry>>>(
             message_filters::sync_policies::ApproximateTime<
-              sensor_msgs::msg::PointCloud2,
+              sensor_msgs::msg::LaserScan,
               nav_msgs::msg::Odometry>(queue_size),
-            sub_pcd,
-            sub_pose);
+            sub_scan,
+            sub_odom);
       sync->registerCallback(std::bind(
-          &TransformerNode::callback_pc2_registration,
+          &TransformerNode::callback_scan_registration,
           this,
           std::placeholders::_1,
           std::placeholders::_2));
@@ -189,7 +186,7 @@ class TransformerNode  : public rclcpp::Node
 };
 
 
-int init_lasecan_transformer_node(int argc, char** argv)
+int init_laserscan_transformer_node(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
 
