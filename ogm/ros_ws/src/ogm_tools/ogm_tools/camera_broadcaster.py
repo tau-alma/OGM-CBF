08a8545e5514c013c2735f37a8938492a1e1c845
Milos Pragr <milos.pragr@tuni.fi>
2026-01-14 14:11:44 +0200

    update br

--- ogm/ros_ws/src/ogm_tools/ogm_tools/camera_broadcaster.py
+++ ogm/ros_ws/src/ogm_tools/ogm_tools/camera_broadcaster.py
@@ -36,40 +36,39 @@ class CameraBroadcaster(Node):
 
     def tick(self):
         
-        if self.t_map_2_tag_link is None or self.t_tag_2_camera is None:
             
-            try:
-                self.t_map_2_tag_link = self.tf_buffer.lookup_transform(
-                        self.tag_link_frame,
-                        self.map_frame,
-                        rclpy.time.Time())
-            except TransformException as ex:
-                self.get_logger().info("waiting for %s --> %s" % (self.map_frame, self.tag_link_frame))
-                self.t_map_2_tag_link = None
+        try:
+            self.t_map_2_tag_link = self.tf_buffer.lookup_transform(
+                    self.tag_link_frame,
+                    self.map_frame,
+                    rclpy.time.Time())
+        except TransformException as ex:
+            self.get_logger().info("waiting for %s --> %s" % (self.map_frame, self.tag_link_frame))
+            self.t_map_2_tag_link = None
 
-            try:
-                self.t_tag_2_camera = self.tf_buffer.lookup_transform(
-                        self.camera_frame,
-                        self.tag_frame,
-                        rclpy.time.Time())
-            except TransformException as ex:
-                self.get_logger().info("waiting for %s --> %s" % (self.tag_frame, self.camera_frame))
-                self.t_tag_2_camera = None
-            
-            if self.t_map_2_tag_link is not None and self.t_tag_2_camera is not None:
-                t_map_2_connect = TransformStamped()
-                t_map_2_connect.header.stamp = self.get_clock().now().to_msg()
-                t_map_2_connect.header.frame_id = self.map_frame
-                t_map_2_connect.child_frame_id = self.tag_init_frame
-                t_map_2_connect.transform = self.t_map_2_tag_link.transform
-                print (t_map_2_connect)
-                self.tf_static_broadcaster.sendTransform(t_map_2_connect)
+        try:
+            self.t_tag_2_camera = self.tf_buffer.lookup_transform(
+                    self.camera_frame,
+                    self.tag_frame,
+                    rclpy.time.Time())
+        except TransformException as ex:
+            self.get_logger().info("waiting for %s --> %s" % (self.tag_frame, self.camera_frame))
+            self.t_tag_2_camera = None
+        
+        if self.t_map_2_tag_link is not None and self.t_tag_2_camera is not None:
+            t_map_2_connect = TransformStamped()
+            t_map_2_connect.header.stamp = self.get_clock().now().to_msg()
+            t_map_2_connect.header.frame_id = self.map_frame
+            t_map_2_connect.child_frame_id = self.tag_init_frame
+            t_map_2_connect.transform = self.t_map_2_tag_link.transform
+            print (t_map_2_connect)
+            self.tf_static_broadcaster.sendTransform(t_map_2_connect)
 
-                #t_connect_2_camera = self.t_tag_2_camera
-                #t_connect_2_camera.header.frame_id = self.tag_init_frame
-                #self.tf_static_broadcaster.sendTransform(t_connect_2_camera)
+            #t_connect_2_camera = self.t_tag_2_camera
+            #t_connect_2_camera.header.frame_id = self.tag_init_frame
+            #self.tf_static_broadcaster.sendTransform(t_connect_2_camera)
 
-                self.get_logger().info("%s --> %s static init done" % (self.map_frame, self.camera_frame))
+            self.get_logger().info("%s --> %s static init done" % (self.map_frame, self.camera_frame))
             
 
 def main(args=None):
