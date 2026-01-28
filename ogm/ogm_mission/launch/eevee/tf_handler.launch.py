8d29de723fcd4885e46d562231d8baeacdefd75f
Milos Pragr <milos.pragr@tuni.fi>
2026-01-28 15:37:50 +0200

    eevee tf

---
+++ ogm/ogm_mission/launch/eevee/tf_handler.launch.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import os
+import yaml
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    _use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='use_sim_time'
+    )
+
+    _ns = DeclareLaunchArgument(
+        'ns',
+        default_value='eevee_tf_handler',
+        description='tf handler namespace'
+    )
+
+
+    offset_odom = Node(
+            package="ogm_gridmap",
+            executable="offset_odom",
+            name="odom_2_map" ,
+            output='screen' ,
+            namespace=LaunchConfiguration('ns'),
+            remappings=[
+                ('odom_in' , '/fixposition/odometry_enu'),
+                ('odom_out' , '/odom_in_map'),
+                ],
+            parameters=[
+                {"use_sim_time" : LaunchConfiguration('use_sim_time')},
+                {'map_frame' : 'map'},
+                {'link_frame' : 'base_link'},
+                {'offset_to_link' : False},
+                {'from_pose' : False},
+                {'from_params' : True},
+                {'fixed_offset_x' : 0.},
+                {'fixed_offset_y' : 0.},
+                ],
+            ) 
+
+    odom_2_tf = Node(
+            package="odom_to_tf_ros2",
+            executable="odom_to_tf",
+            name="odom_2_tf" ,
+            output={'both': 'log'} ,
+            namespace=LaunchConfiguration('ns'),
+            parameters=[
+                {"use_sim_time" : LaunchConfiguration('use_sim_time')},
+                {'odom_topic' : '/odom_in_map'},
+                ],
+            ) 
+
+    sbr_base_link_2_velarray = Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="sbr_base_link_2_front_laser_link",
+            arguments=["-1.2", "0.0", "0.4",
+                       " 3.14159265", "0.11392", "-0.0361111",
+                       "base_link", "velarray"],
+            namespace=LaunchConfiguration('ns'),
+            parameters=[
+                {"use_sim_time" : LaunchConfiguration('use_sim_time')},
+                ],
+            ) 
+
+
+    return LaunchDescription([
+        _use_sim_time,
+        _ns,
+        offset_odom,
+        odom_2_tf,
+        sbr_base_link_2_velarray,
+    ])
